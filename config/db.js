ed07df2cce30d668eae7d383f4ace5aed1faeea8
kais-120 <121316071+kais-120@users.noreply.github.com>
2025-08-01 14:08:37 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('djerba_coworking', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize({
+  username:process.ENV.MYSQLUSER,
+  password:process.ENV.MYSQLPASSWORD,
+  port:process.ENV.MYSQLPORT,
+  host: process.ENV.MYSQLHOST,
+  database:process.ENV.MYSQLDATABASE,
   dialect: 'mysql',
   logging: false
 });
