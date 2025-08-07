4eff316bd0a986bf859b8a8438a6ec00e0bd4377
kais-120 <kaisbennour7@gmail.com>
2025-08-07 15:08:25 +0100

    fix error

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const AuthRoutes = require("./routes/authRoutes");
 const UsersRoutes = require("./routes/usersRoutes")
-const SpaceRoutes = require("./routes/SpaceRoutes")
-const EquipmentRoutes = require("./routes/EquipmentRoutes");
 const BookingRoutes = require("./routes/bookingRoutes")
 const PaymentRoutes = require("./routes/PaymentRoutes")
 const ReviewRoutes = require("./routes/ReviewRoutes")
@@ -12,10 +10,8 @@ const NewsRoutes = require("./routes/NewsRoutes")
 
 
 router.use("/auth",AuthRoutes);
-router.use("/space",SpaceRoutes);
 router.use("/booking",BookingRoutes);
 router.use("/user",UsersRoutes);
-router.use("/equipment",EquipmentRoutes);
 router.use("/payment",PaymentRoutes);
 router.use("/review",ReviewRoutes);
 router.use("/dashboard",DashboardRoutes);
